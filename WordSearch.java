de027f0d8ad6c8bf434f8ec714bad7b2fb0efb6b
nikhil-kulkarni <nikhil896@gmail.com>
2015-12-17 16:08:14 -0500

    Re-add wordsearch

--- WordSearch.java
+++ WordSearch.java
@@ -5,6 +5,26 @@
 
 public class WordSearch {
     public static void main(String[] args) {
-        
+        char[][] board = {{'a', 'b', 'c', 'b'}, {'a', 'q', 'd', 'e'}};
+        System.out.println(exists(board, "abcde", 0, 0, 0));
+    }
+
+    public static boolean exists(char[][] board, String str, int i, int j, int k) {
+        if (i >= board.length || j >= board[0].length || i < 0 || j < 0) {
+            return false;
+        }
+        if (board[i][j] == str.charAt(k)) {
+            if (k == str.length() - 1) {
+                return true;
+            }
+            char temp = board[i][j];
+            board[i][j] = '#';
+            if (exists(board, str, i + 1, j, k + 1) || exists(board, str, i, j + 1, k + 1) ||
+                exists(board, str, i - 1, j, k + 1) || exists(board, str, i, j - 1, k + 1)) {
+                    return true;
+            }
+            board[i][j] = temp;
+        }
+        return false;
     }
 }
