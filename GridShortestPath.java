3dfcc1696ae974b84f15516a4789fe65cc46bd9c
nikhil-kulkarni <nikhil896@gmail.com>
2015-12-19 15:59:43 -0500

    Longest repeating subsequence

--- GridShortestPath.java
+++ GridShortestPath.java
@@ -5,14 +5,21 @@ public class GridShortestPath {
     }
 
     public static int minPath(int[][] grid, int xStart, int yStart, int xEnd, int yEnd, int n) {
-        if (xStart < 0 || yStart < 0 || xStart > grid.length || yStart > grid[0].length) {
+        if (xStart < 0 || yStart < 0 || xStart >= grid.length || yStart >= grid[0].length) {
+            return 0;
+        }
+        int ways = 0;
+        if (grid[xStart][yStart] == -1) {
             return 0;
         }
         if (xStart == xEnd && yStart == yEnd) {
             return n;
         } else {
-            return Math.min(1 + minPath(grid, xStart + 1, yStart, xEnd, yEnd, n + 1), Math.min(1 + minPath(grid, xStart - 1, yStart, xEnd, yEnd, n + 1),
+            grid[xStart][yStart] = -1;
+             ways = Math.min(1 + minPath(grid, xStart + 1, yStart, xEnd, yEnd, n + 1), Math.min(1 + minPath(grid, xStart - 1, yStart, xEnd, yEnd, n + 1),
                 Math.min(1 + minPath(grid, xStart, yStart + 1, xEnd, yEnd, n + 1), 1 + minPath(grid, xStart, yStart - 1, xEnd, yEnd, n + 1))));
+            grid[xStart][yStart] = 0;
         }
+        return ways;
     }
 }
--- LCSII.java
+++ LCSII.java
@@ -4,9 +4,44 @@ public class LCSII {
     public static void main(String[] args) {
         String a = "abcghlawsb"; // "ab" is pattern
         String b = "aabbaa";     // "aa" is pattern
+        String max = "";
+        for (int i = 0; i < a.length(); i++) {
+            String result = lcs(a.substring(0, i), a.substring(i));
+            if (result.length() > max.length()) {
+                max = result;
+            }
+        }
+        System.out.println(max);
     }
 
-    public static String lcs(String str) {
-        
+    public static String lcs(String str, String sub) {
+        int[][] grid = new int[str.length() + 1][sub.length() + 1];
+        grid[0][0] = 0;
+        for (int i = 1; i <= str.length(); i++) {
+            for (int j = 1; j <= sub.length(); j++) {
+                if (str.charAt(i - 1) == sub.charAt(j - 1)) {
+                    grid[i][j] = 1 + grid[i - 1][j - 1];
+                } else {
+                    grid[i][j] = Math.max(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+        int i = grid.length - 1;
+        int j = grid[0].length - 1;
+        StringBuilder pattern = new StringBuilder();
+        while (i > 0 && j > 0) {
+            if (str.charAt(i - 1) == sub.charAt(j - 1)) {
+                pattern.append(str.charAt(i - 1));
+                i = i - 1;
+                j = j - 1;
+            } else {
+                if (grid[i - 1][j] > grid[i][j - 1]) {
+                    i = i - 1;
+                } else {
+                    j = j - 1;
+                }
+            }
+        }
+        return pattern.reverse().toString();
     }
 }
