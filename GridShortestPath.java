35ac77003855b5f4808020fbe4a9a21d270e70c5
nikhil-kulkarni <nikhil896@gmail.com>
2015-12-20 17:56:31 -0500

    Fix grid path

--- GridShortestPath.java
+++ GridShortestPath.java
@@ -1,22 +1,23 @@
 public class GridShortestPath {
     public static void main(String[] args) {
-        int[][] grid = new int[3][3];
-        System.out.println(minPath(grid, 0, 0, 2, 2, 0));
+        int[][] grid = new int[3][5];
+        System.out.println(minPath(grid, 0, 0, 2, 4, 0));
     }
 
     public static int minPath(int[][] grid, int xStart, int yStart, int xEnd, int yEnd, int n) {
         if (xStart < 0 || yStart < 0 || xStart >= grid.length || yStart >= grid[0].length) {
-            return 0;
+            return Integer.MAX_VALUE;
         }
         int ways = 0;
         if (grid[xStart][yStart] == -1) {
-            return 0;
+            return Integer.MAX_VALUE;
         }
         if (xStart == xEnd && yStart == yEnd) {
             return n;
         } else {
             grid[xStart][yStart] = -1;
-
+             ways = Math.min(minPath(grid, xStart + 1, yStart, xEnd, yEnd, n + 1), Math.min(minPath(grid, xStart - 1, yStart, xEnd, yEnd, n + 1),
+                Math.min(minPath(grid, xStart, yStart + 1, xEnd, yEnd, n + 1), minPath(grid, xStart, yStart - 1, xEnd, yEnd, n + 1))));
             grid[xStart][yStart] = 0;
         }
         return ways;
--- Robot.java
+++ Robot.java
@@ -2,7 +2,7 @@ public class Robot {
     public static void main(String[] args) {
         int x = 3;
         int y = 4;
-        System.out.println(dfs(x, y, 0, 0));
+        System.out.println(robotDP(x, y, 0, 0));
     }
 
     public static int dfs(int x, int y, int currX, int currY) {
@@ -15,12 +15,19 @@ public class Robot {
         return dfs(x, y, currX + 1, currY) + dfs(x, y, currX, currY + 1);
     }
 
-    public static int robotDP(int x, int y) {
-        int[][] grid = new int[x][y];
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-
+    public static int robotDP(int x, int y, int z, int q) {
+        int[][] grid = new int[x+1][y+1];
+        for (int i = 0; i <= x; i++) {
+            grid[i][0] = 1;
+        }
+        for (int i = 0; i <= y; i++) {
+            grid[0][i] = 1;
+        }
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
+                grid[i][j] = grid[i-1][j] + grid[i][j-1];
             }
         }
+        return grid[x][y];
     }
 }
